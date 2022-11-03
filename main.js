de5e94004ba13bae163307987c3d6d3a69ad6c12
deborapianezzer <deboradpianezzer@gmail.com>
2022-11-03 16:41:09 -0300

    realizando alterações no main.js e css.

--- main.js
+++ main.js
@@ -17,25 +17,17 @@ function createCard(date, day, games){
   </div>
   `
 }
-document.querySelector("#app").innerHTML = `
-  <header>
-    <img src="./assets/logo.svg" alt="Logo NLWCopa">
-  </header>
-  <main id="cards">
-    ${createCard("20/11", "domingo", 
-    createGame("qatar", "13:00", "ecuador"))}
+document.querySelector("#cards").innerHTML = 
+    createCard("20/11", "domingo", 
+    createGame("qatar", "13:00", "ecuador")) +
 
-    ${createCard("21/11","segunda",
-      createGame("england", "10:00", "iran") +
-      createGame("senegal", "13:00", "netherlands") +
-      createGame("unitedstates", "16:00", "welsh")
-    )}
+    createCard("21/11","segunda",
+    createGame("england", "10:00", "iran") +
+    createGame("senegal", "13:00", "netherlands") +
+    createGame("unitedstates", "16:00", "welsh")) +
 
-    ${createCard("22/11","terça",
-      createGame("argentina", "07:00", "saudi") +
-      createGame("denmark", "10:00", "tunisia") +
-      createGame("mexico", "13:00", "poland") +
-      createGame("france", "16:00","australia") 
-    )}
-  </main>
-`
+    createCard("22/11","terça",
+    createGame("argentina", "07:00", "saudi") +
+    createGame("denmark", "10:00", "tunisia") +
+    createGame("mexico", "13:00", "poland") +
+    createGame("france", "16:00","australia"))
